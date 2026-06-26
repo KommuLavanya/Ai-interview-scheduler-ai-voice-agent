8370ae0b277fd160a4d10bd3366c0a3385c0411b
unknown <kommulavanya480@gmail.com>
2026-06-26 16:43:36 +0530

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import {Button} from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Ai Voice Recruiter Agent by Team-AA4</h2>
+    </div>
+  );
+}
